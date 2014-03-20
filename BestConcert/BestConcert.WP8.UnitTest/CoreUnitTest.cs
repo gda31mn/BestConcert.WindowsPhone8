3567facf44cb660a5c0e36322c967e7f0b617f68
Riquium <xavier.giovannacci@gmail.com>
2014-03-20 14:51:13 +0100

    Correction bug API

---
+++ BestConcert/BestConcert.WP8.UnitTest/CoreUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestConcert.WP8.Core.Provider;
+using NUnit.Framework;
+
+namespace BestConcert.WP8.UnitTest
+{
+    [TestFixture]
+    public class CoreUnitTest
+    {
+        [Test]
+        public async void SignInManagementProvider()
+        {
+            var result = await ManagementProvider.SignInAsync("username", "password");
+            Assert.IsNotNull(result);
+        }
+    }
+}
