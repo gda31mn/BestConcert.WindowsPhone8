572af9c3f672168e215146d5907ff5a5d239cf7c
Riquium <xavier.giovannacci@gmail.com>
2014-03-27 15:08:40 +0100

    ViewModel

--- BestConcert/BestConcert.WP8.ViewModel/ViewModel/ValidationViewModel.cs
+++ BestConcert/BestConcert.WP8.ViewModel/ViewModel/ValidationViewModel.cs
@@ -3,12 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace BestConcert.WP8.ViewModel.ViewModel
 {
-    public class ValidationViewModel : ViewModelBase
+    public class ValidationViewModel : BestConcertViewModelBase
     {
+        private string _creditCardNumber;
 
+        public string CreditCardNumber
+        {
+            get { return _creditCardNumber; }
+            set
+            {
+                _creditCardNumber = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private DateTimeOffset _expirationDate;
+
+        public DateTimeOffset ExpirationDate
+        {
+            get { return _expirationDate; }
+            set
+            {
+                _expirationDate = value; 
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand ValidationCommand { get; set; }
+
+        public ValidationViewModel()
+        {
+            ValidationCommand = new RelayCommand(OnClickValidation);
+        }
+
+        private void OnClickValidation()
+        {
+            if (String.IsNullOrEmpty(CreditCardNumber))
+            {
+                
+            }
+        }
     }
 }
